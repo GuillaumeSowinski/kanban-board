94e5bf0c63c229d5dc1d5ee3d02ffd970b62d3cb
Antonin Grenier <drixion05@gmail.com>
2025-07-22 11:31:54 +0200

    fixing classes

--- script.js
+++ script.js
@@ -2,21 +2,41 @@ console.log("Kanban JS loaded...");
 
 // Exemple éventuel de structure
 window.addEventListener("DOMContentLoaded", () => {
-  // Ici, on récupère les éléments du DOM
-  const addCardBtn = document.getElementById("addCardBtn");
-  const searchInput = document.getElementById("searchInput");
-  const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
+	// Ici, on récupère les éléments du DOM
+	const addCardBtn = document.getElementById("addCardBtn");
+	const searchInput = document.getElementById("searchInput");
+	const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
+	const card = document.getElementsByClassName("card");
+  const column = document.getElementByClassName("column");
+  card.draggable = true;
 
-  // Éventuellement, on écoute les événements
-  addCardBtn.addEventListener("click", () => {
-    // ...
+
+	// Éventuellement, on écoute les événements
+	addCardBtn.addEventListener("click", () => {
+		// ...
+	});
+
+	searchInput.addEventListener("input", () => {
+		// ...
+	});
+
+	sortByPriorityBtn.addEventListener("click", () => {
+		// ...
+	});
+
+card.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData('text/plain', card.id);
   });
 
-  searchInput.addEventListener("input", () => {
-    // ...
+column.addEventListener("dragover", (event) => {
+    event.preventDefault();
   });
 
-  sortByPriorityBtn.addEventListener("click", () => {
-    // ...
+column.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const draggingCardId = event.dataTransfer.getData("text/plain");
+    const draggingCard = document.getElementById(draggingCardId);
+    column.appendChild(draggingCard);
   });
+
 });
